25b6cec07ea8d8a169f78edaf34d53d6fbd19ae2
Noah Rosamilia <noah@ivoah.net>
2025-09-10 14:41:24 -0400

    Only search table body

--- static/search.js
+++ static/search.js
@@ -1,6 +1,6 @@
 function search(query) {
   const pattern = new RegExp(query, "i");
-  for (const row of document.querySelectorAll("#movies tr")) {
+  for (const row of document.querySelectorAll("#movies>tbody tr")) {
     if (pattern.test(row.querySelector("a").text)) {
       row.style.display = "";
     } else {
